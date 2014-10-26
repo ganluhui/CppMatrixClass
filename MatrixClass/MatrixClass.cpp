d34cdad53201fd44b3906ea9689bdf3e9641984d
Luke Gan <ganlh6@gmail.com>
2014-10-26 15:34:01 -0400

    definition of basic members

--- MatrixClass/MatrixClass.cpp
+++ MatrixClass/MatrixClass.cpp
@@ -1,5 +1,6 @@
 // MatrixClass.cpp : Defines the entry point for the console application.
-//
+// This project serves as an interface
+// The implementation of the class Matrix will be in the project 'Core'
 
 #include "stdafx.h"
 
