7dc588c79466ca3f3dd3e29819fade3ac05dc726
Luke Gan <ganlh6@gmail.com>
2014-11-04 16:19:44 -0500

    ignore test1.cpp and Test.cpp

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,24 +1,25 @@
-//#include <iostream>
-//#include <cstdlib>
-//#include "Matrix.h"
-//#include "BandedMatrix.h"
-//#include "BandedArray.h"
-//
-//int main(){
-//	Matrix arr1(1, 5, 5);
-//	//Matrix arr2(5, 5, 5);
-//	//Matrix arr3 = arr2 + arr1;
-//	//arr3.Print();
-//	//BandedMatrix arr4(1.0, 4, 1, 1);
-//	//arr4.Print();
-//	//arr4.Change(2.0, 1, 1);
-//	//arr4.Print();
-//	//BandedMatrix arr5(2.0, 4, 1, 1);
-//	//Matrix arr6 = arr4 + arr5;
-//	//Matrix arr7 = arr4 + arr3;
-//	//BandedMatrix arr8(4, 1, 1);
-//	//Matrix arr9 = arr5 + arr8;
-//	//arr8.Print();
-//
-//	return 0;
-//}
+#include <iostream>
+#include <cstdlib>
+#include "Matrix.h"
+#include "BandedMatrix.h"
+#include "BandedArray.h"
+
+int main(){
+	Matrix arr1(1, 5, 5);
+	std::cout << arr1(3, 3);
+	//Matrix arr2(5, 5, 5);
+	//Matrix arr3 = arr2 + arr1;
+	//arr3.Print();
+	//BandedMatrix arr4(1.0, 4, 1, 1);
+	//arr4.Print();
+	//arr4.Change(2.0, 1, 1);
+	//arr4.Print();
+	//BandedMatrix arr5(2.0, 4, 1, 1);
+	//Matrix arr6 = arr4 + arr5;
+	//Matrix arr7 = arr4 + arr3;
+	//BandedMatrix arr8(4, 1, 1);
+	//Matrix arr9 = arr5 + arr8;
+	//arr8.Print();
+
+	return 0;
+}
